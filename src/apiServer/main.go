c88a21c6e98dfd94b0179ce573ea56796e6d678c
ink <jiangink@foxmail.com>
2019-01-29 10:48:19 +0800

    update command cli

--- src/apiServer/main.go
+++ src/apiServer/main.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	// "fmt"
 	"os"
 	"sort"
 
-	// "github.com/namsral/flag"
 	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli"
 )
 
-// var (
-// 	listenAddrApi string
-// )
-
 func main() {
 	app := cli.NewApp()
-	app.Name = "API Server Commandline"
+	app.Name = "kafka Tutorial API Server Commandline"
 	app.Usage = "Run API Server"
 	app.Version = "1.0.0"
 	app.Flags = args()
-	app.Commands = commands()
 	sort.Sort(cli.FlagsByName(app.Flags))
-	sort.Sort(cli.CommandsByName(app.Commands))
-
 	app.Action = action
 	err := app.Run(os.Args)
 	if err != nil {
@@ -40,30 +31,34 @@ func args() []cli.Flag {
 			Usage: "Listen address for api",
 		},
 		cli.StringFlag{
-			Name:  "config, c",
-			Value: "balbalbal",
-			Usage: "test config ...",
+			Name:  "kafka-brokers, kb",
+			Value: "localhost:19092,localhost:29092,localhost:39092",
+			Usage: "Kafka brokers in comma separated value",
 		},
-	}
-}
-
-func commands() []cli.Command {
-	return []cli.Command{
-		{
-			Name:    "complete",
-			Aliases: []string{"c"},
-			Usage:   "complete a task on the list",
-			Action: func(c *cli.Context) error {
-				return nil
-			},
+		cli.BoolFlag{
+			Name:  "kafka-verbose, kv",
+			Usage: "Kafka verbose logging",
+		},
+		cli.StringFlag{
+			Name:  "kafka-cli-id, kci",
+			Value: "kafka-client",
+			Usage: "Kafka client id to connect",
+		},
+		cli.StringFlag{
+			Name:  "kafka-topic, kt",
+			Value: "hello",
+			Usage: "Kafka topic to push",
 		},
 	}
 }
 
 func action(c *cli.Context) error {
-	log.Info().Msg("Commandline Sample.")
-	log.Info().Msg("(c) erik 2019")
+	log.Info().Msg("kafka tutorial api server.")
+	log.Info().Msg("(c) Erik 2019")
 	log.Info().Msgf("listen-address : %s \n", c.String("listen-address"))
-	log.Info().Msgf("config : %s \n", c.String("config"))
+	log.Info().Msgf("kafka-brokers : %s \n", c.String("kafka-brokers"))
+	log.Info().Msgf("kafka-verbose : %t \n", c.Bool("kafka-verbose"))
+	log.Info().Msgf("kafka-cli-id : %s \n", c.String("kafka-cli-id"))
+	log.Info().Msgf("kafka-topic : %s \n", c.String("kafka-topic"))
 	return nil
 }
