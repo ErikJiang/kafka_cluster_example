5c1194e6674417f6b4615e8051b2f1c91472e55a
ink <jiangink@foxmail.com>
2019-01-31 18:09:41 +0800

    update

--- src/consume/main.go
+++ src/consume/main.go
@@ -12,8 +12,7 @@ import (
 
 	"github.com/rs/zerolog/log"
 	"github.com/urfave/cli"
-	"github.com/segmentio/kafka-go"
-	// "github.com/segmentio/kafka-go/snappy"
+	"github.com/bsm/sarama-cluster"
 )
 
 func main() {
@@ -139,4 +138,57 @@ func loop(r *kafka.Reader) error {
 		}
 		log.Debug().Msgf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
 	}
+}
+
+
+// 支持brokers cluster的消费者
+func clusterConsumer(wg *sync.WaitGroup, brokers, topics []string, groupId string)  {
+    defer wg.Done()
+    config := cluster.NewConfig()
+    config.Consumer.Return.Errors = true
+    config.Group.Return.Notifications = true
+    config.Consumer.Offsets.Initial = sarama.OffsetNewest
+ 
+    // init consumer
+    consumer, err := cluster.NewConsumer(brokers, groupId, topics, config)
+    if err != nil {
+        log.Printf("%s: sarama.NewSyncProducer err, message=%s \n", groupId, err)
+        return
+    }
+    defer consumer.Close()
+ 
+    // trap SIGINT to trigger a shutdown
+    signals := make(chan os.Signal, 1)
+    signal.Notify(signals, os.Interrupt)
+ 
+    // consume errors
+    go func() {
+        for err := range consumer.Errors() {
+            log.Printf("%s:Error: %s\n", groupId, err.Error())
+        }
+    }()
+ 
+    // consume notifications
+    go func() {
+        for ntf := range consumer.Notifications() {
+            log.Printf("%s:Rebalanced: %+v \n", groupId, ntf)
+        }
+    }()
+ 
+    // consume messages, watch signals
+    var successes int
+    Loop:
+    for {
+        select {
+        case msg, ok := <-consumer.Messages():
+            if ok {
+                fmt.Fprintf(os.Stdout, "%s:%s/%d/%d\t%s\t%s\n", groupId, msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+                consumer.MarkOffset(msg, "")  // mark message as processed
+                successes++
+            }
+        case <-signals:
+            break Loop
+        }
+    }
+    fmt.Fprintf(os.Stdout, "%s consume %d messages \n", groupId, successes)
 }
